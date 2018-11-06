396889c12217830e65aceb57387d4b0e460fe7c2
SDR48 <41689186+SDR48@users.noreply.github.com>
2018-11-06 23:33:44 +0530

    Basic app to upload a file on Firebase

---
+++ app/src/main/java/com/myapps/sdr/storagefirebase/FetchAct.java
@@ -0,0 +1,13 @@
+package com.myapps.sdr.storagefirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FetchAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fetch);
+    }
+}
